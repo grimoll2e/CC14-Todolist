9327684fd03a5ad8e51d7ce0a8d402be12ef246e
grimoll2e <itmutcpe6@gmail.com>
2023-07-11 21:44:01 +0700

    ui-state toggle add-todo

--- src/components/Todo/Addtodo.jsx
+++ src/components/Todo/Addtodo.jsx
@@ -1,14 +1,21 @@
 import styles from './Addtodo.module.scss'
+import { useState } from 'react'
 import { TodoForm } from './TodoForm'
 
 export function Addtodo() {
+    const [isShow,setIsShow] = useState(false)
+
+    const handleClickToAddTask = () =>{
+        setIsShow(!isShow)
+    }
     return (
         <>
-            <div className={styles.add__todo}>
+            {!isShow?(<div className={styles.add__todo} onClick={handleClickToAddTask}>
                 <span>+</span>
                 <h3>Add task</h3>
-            </div>
-            <TodoForm />
+            </div>):(
+            <TodoForm onSetIsShow={handleClickToAddTask}/>
+            )}
         </>
     )
 }
--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,14 +1,34 @@
 import styles from './TodoForm.module.scss'
+import React,{useState} from 'react';
 
-export function TodoForm() {
+export function TodoForm({onSetIsShow}) {
+
+    const [taks,setTaks] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        onSetIsShow()//จากที่ส่งมาจาก Addtodo
+
+    }
+
+    const handleClickcancel = (e) =>{
+        // console.log(e)
+        onSetIsShow()
+    }
+
+    const handleChange=(e)=>{
+        setTaks(e.target.value)
+    }
+
+
     return (
-        <form action="" className={styles.todo__form__container}>
-            <input className={styles.todo__form__input} placeholder='Task Name' />
+        <form onSubmit={handleSubmit} className={styles.todo__form__container}>
+            <input className={styles.todo__form__input} placeholder='Task Name' value={taks} onChange={handleChange}/>
             <div className={styles.todo__form__footer}>
                 <p className={styles.todo__error}> Title is required</p>
                 <div className={styles.todo__form__buttons}>
-                    <button>Cancle</button>
-                    <button>Add task</button>
+                    <button type='button' onClick={handleClickcancel}>Cancle</button>
+                    <button type='submit'>Add task</button>
                 </div>
 
             </div>
