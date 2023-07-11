b0da539acb567d8a0a3951d796dac6e94362d3eb
grimoll2e <itmutcpe6@gmail.com>
2023-07-11 15:51:17 +0700

    finish:TodoHeader time

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,11 +1,16 @@
-
+import styles from './TodoContent.module.scss' //module css
 
 export function TodoContent() {
+
     const mockTodo = Array.from({ length: 50 }, (el, idx) => idx + 1);
+    const now =new Date()
+    const options = { weekday: 'short', month: 'long', day: 'numeric' };
+    console.log(now.toLocaleDateString('en-US',options))
     return (
         <main className="content">
-            <div>
-                <header></header>
+            <div className={styles.header}>
+                <header>Inbox</header>
+                <p>{now.toLocaleDateString('en-US',options)}</p>
             </div>
             <div>
                 addtodo
